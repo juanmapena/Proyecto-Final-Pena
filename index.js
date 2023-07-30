ab4f3dfaa43df31b872dfa0232a2f4b0f40f7186
juanmapena <juanmanuelpena180@gmail.com>
2023-07-30 15:30:03 -0300

    se crearon objetos, arrays, array de objetos, funciones de orden superior, metodos de busqueda

--- index.js
+++ index.js
@@ -40,6 +40,25 @@ let promedio = (nota1 + nota2) / 2
 //llamamos a la funcion y le pasamos los parametros de nota1 y nota2, pero como la funcion nos retorna 2 valores, guardamos esos 2 valores en 2 variables e igualamos a la funcion
 let condicion2 = condicion(promedio)
 
-console.log ("El estudiante " +  nombre + ", obtuvo un promedio de " + promedio + " por lo que su condición actual es: " + condicion2 + ".")
+
+//Creamos un array vacio, donde dentro se creara un objeto para cada estudiante, con su respectiva informacion
+const estudiantes = [];
+
+const estudiante = {
+    nombre,
+    promedio,
+    condicion2,
+};
+
+
+estudiantes.push (estudiante)
+
+//Se consologuea los resultados finales de cada alumno, mediante un metodo de busqueda (forEach en este caso) se retorna la informacion del array
+console.log("Resultados Finales de cada alumno:");
+estudiantes.forEach((estudiante) => {
+    console.log( "El estudiante " + estudiante.nombre + " obtuvo un Promedio de: " + estudiante.promedio + ", por lo tanto su condición actual es: " + estudiante.condicion2);
+}
+
+)
 
 }
