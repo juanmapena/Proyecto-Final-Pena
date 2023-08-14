d7c59b0128d74cde10058ef97acc6cd1b433d5e4
juanmapena <juanmanuelpena180@gmail.com>
2023-08-14 15:07:58 -0300

    se crearon eventos funcionales, uniendolo con DOM

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
         const calificacion1 = parseFloat(document.getElementById("calificacion1Alumnos").value);
         const calificacion2 = parseFloat(document.getElementById("calificacion2Alumnos").value);
 
+        //En caso de que no sea un numero:
         if (isNaN(calificacion1) || isNaN(calificacion2) || calificacion1 < 0 || calificacion1 > 10 || calificacion2 < 0 || calificacion2 > 10) {
             mostrarError("Las calificaciones ingresadas no son válidas. Por favor, ingrese valores entre 0 y 10.");
             return;
         }
-
+        //En caso de que el nombre sea otra cosa distinta a un nombre:
         if (siEsUnNumero(nombre)) {
             mostrarError("El nombre ingresado no es válido. Por favor, ingrese un nombre válido.");
             return;
@@ -25,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Limpia el contenido anterior en caso de que haya resultados previos
         resultadosElement.innerHTML = "";
 
-        // Mostrar los resultados en el elemento 'resultados'
+        // Mostrar los resultados en el elemento "resultados"
         const estudianteResult = document.createElement("p");
         estudianteResult.textContent =
             "El estudiante " +
